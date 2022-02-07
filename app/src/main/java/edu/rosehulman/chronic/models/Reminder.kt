0caab22f5924c696a0f58c6bd21ccd68dcb2532a
rhit-koenignm <70714241+rhit-koenignm@users.noreply.github.com>
2022-02-06 22:56:05 -0500

    Reminder view model all set up and reminders added to

--- app/src/main/java/edu/rosehulman/chronic/models/Reminder.kt
+++ app/src/main/java/edu/rosehulman/chronic/models/Reminder.kt
@@ -1,16 +1,38 @@
 package edu.rosehulman.chronic.models
 
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.Exclude
 
-data class Reminder(
-    var title: String,
-    var content: String,
-    var hour: Int,
-    var minute: Int
-) {
-    var isActive: Boolean = true
+class Reminder(
+    var title: String = "Default title",
+    var content: String = "Default content",
+    var hours: Int = 0,
+    var minutes: Int = 0,
+    var isActive: Boolean = true,
     var daysActive: List<Boolean> = listOf(true, true, true, true, true)
+) {
 
     @get:Exclude
     var id = ""
+
+    fun makeACopy(): Reminder {
+        var clone = Reminder()
+        clone.title = title
+        clone.content = content
+        clone.hours = hours
+        clone.minutes = minutes
+        clone.isActive = isActive
+        clone.daysActive = ArrayList(daysActive)
+        return clone
+    }
+
+    companion object {
+        const val COLLECTION_PATH = "reminders"
+
+        fun from(snapshot: DocumentSnapshot): Reminder {
+            val reminder = snapshot.toObject(Reminder::class.java)!!
+            reminder.id = snapshot.id
+            return reminder
+        }
+    }
 }
--- app/src/main/java/edu/rosehulman/chronic/models/ReminderViewModel.kt
+++ app/src/main/java/edu/rosehulman/chronic/models/ReminderViewModel.kt
@@ -1,4 +1,103 @@
 package edu.rosehulman.chronic.models
 
-class ReminderViewModel {
+import android.provider.SyncStateContract
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.firestoreSettings
+import com.google.firebase.ktx.Firebase
+import edu.rosehulman.chronic.Constants
+
+
+// Author: Natalie Koenig
+// Description: The view model class to be used in the ReminderAdapter that will show up in the reminders page
+// Date: 2/6/2022
+class ReminderViewModel : ViewModel() {
+
+    //The reminders will be stored in an arraylist
+    var reminders = ArrayList<Reminder>()
+    var currentPos = 0
+
+    fun getReminderAt(pos: Int) = reminders[pos]
+    fun getCurrentReminder() = reminders[currentPos]
+
+    var uid = Firebase.auth.uid!!
+    var ref = Firebase
+        .firestore
+        .collection(UserData.COLLECTION_PATH)
+        .document(uid)
+        .collection(Reminder.COLLECTION_PATH)
+
+    var subscriptions = HashMap<String, ListenerRegistration>()
+
+    fun addListener(fragmentName: String, observer: () -> Unit) {
+        Log.d(Constants.TAG, "Adding reminder listener for fragment $fragmentName")
+
+        val subscription = ref
+            .orderBy("hours", Query.Direction.ASCENDING)
+            .orderBy("minutes", Query.Direction.ASCENDING)
+            .addSnapshotListener { snapshot: QuerySnapshot?, error: FirebaseFirestoreException? ->
+                error?.let {
+                    Log.d(Constants.TAG, "Error: $error")
+                    return@addSnapshotListener
+                }
+                Log.d(Constants.TAG, "In snaphot listener with ${snapshot?.size()}")
+                reminders.clear()
+                snapshot?.documents?.forEach {
+                    reminders.add(Reminder.from(it))
+                }
+                observer()
+            }
+        subscriptions[fragmentName] = subscription
+    }
+
+    fun removeListener(fragmentName: String) {
+        Log.d(Constants.TAG, "Removing listener for $fragmentName")
+        subscriptions[fragmentName]?.remove()
+        subscriptions.remove(fragmentName)
+    }
+
+    fun addReminder(reminder: Reminder?) {
+        if (reminder == null) {
+            var newReminder = Reminder()
+            ref.add(newReminder)
+        }
+        else {
+            ref.add(reminder.makeACopy())
+        }
+    }
+
+    fun updateCurrentReminder(reminder: Reminder?) {
+        if (reminder != null) {
+            reminders[currentPos] = reminder
+            ref.document(getCurrentReminder().id).set(getCurrentReminder())
+        }
+    }
+
+    fun removeReminder() {
+        ref.document(getCurrentReminder().id).delete()
+        currentPos = 0
+    }
+
+    fun updatePos(pos: Int) {
+        currentPos = pos
+    }
+
+    fun clear() {
+        reminders.clear()
+    }
+
+    fun size() = reminders.size
+
+    fun toggleCurrentReminder() {
+        reminders[currentPos].isActive = !reminders[currentPos].isActive
+        ref.document(getCurrentReminder().id).set(reminders[currentPos])
+    }
+
+
 }
