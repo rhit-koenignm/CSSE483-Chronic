86959c096e25020fbd46c5a166737770f6022baf
rhit-koenignm <70714241+rhit-koenignm@users.noreply.github.com>
2022-02-14 19:15:44 -0500

    Added listener functionality

--- app/src/main/java/edu/rosehulman/chronic/models/ReplyViewModel.kt
+++ app/src/main/java/edu/rosehulman/chronic/models/ReplyViewModel.kt
@@ -1,10 +1,12 @@
 package edu.rosehulman.chronic.models
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import edu.rosehulman.chronic.utilities.Constants
 
 class ReplyViewModel : ViewModel() {
     var uid = Firebase.auth.uid!!
@@ -20,6 +22,35 @@ class ReplyViewModel : ViewModel() {
     val subscriptions = HashMap<String, ListenerRegistration>()
 
     fun addListener(fragmentName: String, postID: String, observer: () -> Unit) {
+        ref = Firebase
+            .firestore
+            .collection(Post.COLLECTION_PATH)
+            .document(postID)
+            .collection(Reply.COLLECTION_PATH)
+        Log.d(Constants.TAG, "Adding listener for $fragmentName")
+        val subscription = ref
+            .orderBy(Reply.CREATED_KEY, Query.Direction.ASCENDING)
+            .addSnapshotListener { snapshot: QuerySnapshot?, error:FirebaseFirestoreException? ->
+                error?.let {
+                    Log.d(Constants.TAG, "Error: $error")
+                    return@addSnapshotListener
+                }
+                Log.d(Constants.TAG, "In snapshot listener with ${snapshot?.size()} docs")
+                replies.clear()
+                snapshot?.documents?.forEach {
+                    replies.add(Reply.from(it))
+                }
+                observer()
+            }
+        subscriptions[fragmentName] = subscription
+    }
 
+    fun removeListener(fragmentName: String) {
+        Log.d(Constants.TAG, "Removing listener for $fragmentName")
+        subscriptions[fragmentName]?.remove()
+        subscriptions.remove(fragmentName)
+    }
+
+    fun addReply(reply: Reply?) {
     }
 }
